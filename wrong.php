2db3d3d5296e43693edec656ca191368e42c6fb5
Tran Quoc Viet <tqviet@yrglm-vn.com>
2026-04-29 11:58:04 +0700

    add

--- wrong.php
+++ wrong.php
@@ -4,3 +4,4 @@ $password = "123456"; // cố tình sai
 $API = "123456"; // cố tình sai
 $dmomain = "abc.com"; // cố tình sai
 $dmomain2 = "abc.com"; // cố tình sai
+$dmomain3 = "abc.com"; // cố tình sai
